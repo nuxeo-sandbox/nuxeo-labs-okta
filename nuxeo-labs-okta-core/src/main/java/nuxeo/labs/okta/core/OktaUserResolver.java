b2dcae948e18766a796f7a97585c26a89bd40290
Josh Fletcher <jfletcher@nuxeo.com>
2017-01-12 17:48:34 -0800

    Add resolver skeleton.

---
+++ nuxeo-labs-okta-core/src/main/java/nuxeo/labs/okta/core/OktaUserResolver.java
@@ -0,0 +1,25 @@
+package nuxeo.labs.okta.core;
+
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.platform.auth.saml.SAMLCredential;
+import org.nuxeo.ecm.platform.auth.saml.user.AbstractUserResolver;
+
+/**
+ * Created by jfletcher on 1/12/17.
+ */
+public class OktaUserResolver extends AbstractUserResolver {
+    @Override
+    public String findNuxeoUser(SAMLCredential samlCredential) {
+        return null;
+    }
+
+    @Override
+    public String getLoginName(SAMLCredential samlCredential) {
+        return null;
+    }
+
+    @Override
+    public DocumentModel updateUserInfo(DocumentModel documentModel, SAMLCredential samlCredential) {
+        return null;
+    }
+}
